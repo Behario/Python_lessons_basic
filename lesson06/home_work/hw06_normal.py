5e23b85d24815300db728e4326ebc5dc23a5d048
Behario <ezetoanton@gmail.com>
2019-03-03 18:31:51 +0300

    Пока думаю над реализацией

--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,41 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class People:
+    def __init__(self, surname, n_p):
+        self.surname = surname
+        self.n_p = n_p
+
+
+class Teacher(People):
+    def __init__(self, surname, n_p, class_room, subject):
+        People. __init__(self, surname, n_p)
+        self._class_room = class_room
+        self._subject = subject
+
+
+class Pupil(People):
+    def __init__(self, surname, n_p, class_room, subjects):
+        People. __init__(self, surname, n_p)
+        self._class_room = {"class_num": int(class_room.split()[0]),
+                            "class_char": class_room.split()[1]}
+        self._subjects = subjects
+
+    @property
+    def get_room(self):
+        return f"{self.get_name} \n" \
+            f"{self._class_room['class_num']} {self._class_room['class_char']}"
+
+    @property
+    def get_name(self):
+        return self.surname + " " + self.n_p
+
+
+pupil1 = Pupil("Косцов", "А. С.", "7 A", ["математика", "русский", "география"])
+pupil2 = Pupil("Стасенко", "А. В.", "3 А", ["черчение", "русский", "география"])
+pupil2 = Pupil("Валерьянка", "Ю. А.", "5 б", ["математика", "русский", "география"])
+
+teacher1 = Teacher("Касяьнов", "А. В.")
+
+print(f"{pupil1.get_room}")
