2c4dd51e293693e70d090eab6ac8dca13d81b4b9
Behario <ezetoanton@gmail.com>
2019-03-04 22:56:08 +0300

    Задача нормал

--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -16,40 +16,114 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
-class People:
-    def __init__(self, surname, n_p):
-        self.surname = surname
-        self.n_p = n_p
+
+class Teacher:
+    def __init__(self, fio, lesson, classes_room):
+        self.fio = fio
+        self.lesson = lesson
+        self.classes_room = classes_room
 
 
-class Teacher(People):
-    def __init__(self, surname, n_p, class_room, subject):
-        People. __init__(self, surname, n_p)
-        self._class_room = class_room
-        self._subject = subject
-
-
-class Pupil(People):
-    def __init__(self, surname, n_p, class_room, subjects):
-        People. __init__(self, surname, n_p)
-        self._class_room = {"class_num": int(class_room.split()[0]),
-                            "class_char": class_room.split()[1]}
-        self._subjects = subjects
+class Pupil:
+    def __init__(self, fio,  father, mother, classroom):
+        self.fio = fio
+        self.father = father
+        self.mother = mother
+        self.classroom = classroom
 
     @property
-    def get_room(self):
-        return f"{self.get_name} \n" \
-            f"{self._class_room['class_num']} {self._class_room['class_char']}"
+    def get_pupil_parents(self):
+        return f"Родители ученика {self.fio}: отец - {self.father}, мать - {self.mother}"
+
+
+class School:
+    def __init__(self, num, city, sch_rooms):
+        self.num = num
+        self.city = city
+        self.sch_rooms = sch_rooms
 
     @property
-    def get_name(self):
-        return self.surname + " " + self.n_p
+    def get_all_rooms(self):
+        return self.sch_rooms
 
 
-pupil1 = Pupil("Косцов", "А. С.", "7 A", ["математика", "русский", "география"])
-pupil2 = Pupil("Стасенко", "А. В.", "3 А", ["черчение", "русский", "география"])
-pupil2 = Pupil("Валерьянка", "Ю. А.", "5 б", ["математика", "русский", "география"])
+def get_pupil_name(name):
+    for i in range(len(pupils)):
+        if pupils[i].fio == name:
+            return pupils[i].get_pupil_parents
+        elif pupils[i].fio != name:
+            continue
+    return "У нас нет такого ученика"
 
-teacher1 = Teacher("Касяьнов", "А. В.")
 
-print(f"{pupil1.get_room}")
+def get_pupils(class_room):
+    pupils_list = []
+    for i in range(len(pupils)):
+        if pupils[i].classroom == class_room:
+            pupils_list.append(pupils[i].fio)
+        else:
+            continue
+    pupils_list = ", ".join(pupils_list)
+    return f"В классе {class_room} есть следующие ученики:\n" \
+        f"{pupils_list}"
+
+
+def get_subjects(name):
+    teach = []
+    subj = []
+    room = ""
+    for i in range(len(pupils)):
+        if pupils[i].fio == name:
+            room += pupils[i].classroom
+            for k in range(len(teachers)):
+                if pupils[i].classroom in teachers[k].classes_room:
+                    subj.append(teachers[k].lesson)
+                    teach.append(teachers[k].fio)
+        elif pupils[i].fio == name:
+            continue
+    teach = ", ".join(teach)
+    subj = ", ".join(subj)
+    return f"Ученик: {name} --- Класс: {room} --- " \
+        f"Проподаватели: {teach} --- Предметы: {subj}"
+
+
+def get_teachers(classroom):
+    teach_class = []
+    for i in range(len(teachers)):
+        if classroom in teachers[i].classes_room:
+            teach_class.append(teachers[i].fio)
+        elif classroom not in teachers[i].classes_room:
+            continue
+    teach_class = ", ".join(teach_class)
+    return f"В классе {classroom} преподают учителя: {teach_class}"
+
+
+rooms_a = [str(i+1)+" А" for i in range(10)]
+rooms_b = [str(i+1)+" Б" for i in range(10)]
+school_rooms = rooms_a + rooms_b
+
+school = School("115", "Минск", school_rooms)
+
+teachers = [Teacher("Апимахович А. А.", "Русский язык", ["5 А", "7 А"]),
+            Teacher("Сапковский А. О.", "Математика", ["5 Б", "9 А"]),
+            Teacher("Руанов В. Ю.", "Физика", ["8 А", "7 А"])]
+
+
+pupils = [Pupil("Косцов А. С.", "Косцов С. Н.", "Косцова Л. К.", "7 А"),
+          Pupil("Косцов С. С.", "Косцов С. Н.", "Косцова Л. К.", "8 А"),
+          Pupil("Добрин Н. Н.", "Добрин А. В.", "Добрина В. С.", "7 А"),
+          Pupil("Варатов С. К.", "Варатов В. Ю.", "Варатова Е. С.", "9 А")]
+
+name_pupil = input("Введите фамилию ученика в формате - Фамилия И. О.: ")
+print(get_pupil_name(name_pupil))
+print("----------------------------")
+print(f"В школе есть следующие классы:\n{school.get_all_rooms}")
+print("----------------------------")
+name_classroom = input("Введите название класса для получения списка учеников: ")
+print(get_pupils(name_classroom))
+print("----------------------------")
+name_pupil1 = input("Введите имя ученика, чтобы узнать, в каком он классе: ")
+print(get_subjects(name_pupil1))
+print("----------------------------")
+name_class = input("Введите название класса, чтобы узнать всех преподавателей в нем: ")
+print(get_teachers(name_class))
